8d29c8a28add301bb96a7b285c1dc0ec7245e27d
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-10-24 01:39:21 +0300

    discriminant

---
+++ Lesson_1/discriminant.rb
@@ -0,0 +1,30 @@
+puts 'Квадратное алгебраическое уравнение имеет вид: ах^2 + bx + c = 0'
+
+print "Введите коэффициент 'a': "
+coef_a = gets.chomp.to_f
+
+print "Введите коэффициент 'b': "
+coef_b = gets.chomp.to_f
+
+print "Введите коэффициент 'c': "
+coef_c = gets.chomp.to_f
+
+puts 'Дискриминант вывычисляется по формуле: D = b^2 – 4ac'
+
+d = coef_b**2 - 4 * coef_a * coef_c
+
+if d.positive?
+  x_1 = ((- coef_b + Math.sqrt(d)) / 2 * coef_a).round(2)
+  x_2 = ((- coef_b - Math.sqrt(d)) / 2 * coef_a).round(2)
+
+  puts "Коэффициент Х1 = #{x_1}"
+  puts "Коэффициент Х2 = #{x_2}"
+  puts "Дискриминант равен: #{d}"
+elsif d.zero?
+  x_1 = ((- coef_b + Math.sqrt(d)) / 2 * coef_a).round(2)
+
+  puts "Коэффициент Х = #{x_1}"
+  puts "Дискриминант равен: #{d}"
+else
+  puts 'Корней нет'
+end
