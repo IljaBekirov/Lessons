27fbf79a1531b78c532a7f5f20a5f62b9585cdfb
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-10-24 02:27:55 +0300

    refactore right triangle and weight

--- Lesson_1/right_triangle.rb
+++ Lesson_1/right_triangle.rb
@@ -1,3 +1,13 @@
+def sides(max, side_one, side_two)
+  max_side_2 = (max**2).round(2)
+  sum_sqr = (side_one**2 + side_two**2).round(2)
+  {
+    max_side: max,
+    max_side_2: max_side_2,
+    sum_sqr: sum_sqr
+  }
+end
+
 print 'Введите сторону треугольника A: '
 side_a = gets.chomp.to_f
 
@@ -7,21 +17,19 @@ side_b = gets.chomp.to_f
 print 'Введите сторону треугольника C: '
 side_c = gets.chomp.to_f
 
-if side_a > side_b && side_a > side_c
-  max_side = side_a
-  max_side_2 = (max_side**2).round(2)
-  sum_sqr = (side_b**2 + side_c**2).round(2)
-elsif side_b > side_a && side_b > side_c
-  max_side = side_b
-  max_side_2 = (max_side**2).round(2)
-  sum_sqr = (side_a**2 + side_c**2).round(2)
-else
-  max_side = side_c
-  max_side_2 = (max_side**2).round(2)
-  sum_sqr = (side_a**2 + side_b**2).round(2)
-end
+hash = if side_a > side_b && side_a > side_c
+         sides(side_a, side_b, side_c)
+       elsif side_b > side_a && side_b > side_c
+         sides(side_b, side_a, side_c)
+       else
+         sides(side_c, side_a, side_b)
+       end
 
-puts 'Треугольник является равносторонним' if side_a == side_b && side_b == side_c
-puts 'Треугольник является равнобедренным' if side_a == side_b || side_a == side_c || side_b == side_c
-puts 'Треугольник является прямоугольным' if max_side_2 == sum_sqr
-puts "Максимальная сторона равна #{max_side}"
+if side_a == side_b && side_b == side_c
+  puts 'Треугольник является равносторонним'
+end
+if side_a == side_b || side_a == side_c || side_b == side_c
+  puts 'Треугольник является равнобедренным'
+end
+puts 'Треугольник является прямоугольным' if hash[:max_side_2] == hash[:sum_sqr]
+puts "Максимальная сторона равна #{hash[:max_side]}"
--- Lesson_1/weight.rb
+++ Lesson_1/weight.rb
@@ -4,7 +4,7 @@ name = gets.chomp
 puts "What's your height? "
 height = gets.chomp.to_i
 
-ideal_weight = (height - 110)*1.15
+ideal_weight = (height - 110) * 1.15
 
 if ideal_weight.positive?
   puts "#{name}, your ideal weight is #{ideal_weight.round(2)} kg"
