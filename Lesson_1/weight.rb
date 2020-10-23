ad1853f55357882fb64f9e692897f9dc23d29f46
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-10-23 23:50:35 +0300

    task 1

---
+++ Lesson_1/weight.rb
@@ -0,0 +1,13 @@
+puts "What's your name? "
+name = gets.chomp
+
+puts "What's your height? "
+height = gets.chomp.to_i
+
+ideal_weight = (height - 110)*1.15
+
+if ideal_weight.positive?
+  puts "#{name}, your ideal weight is #{ideal_weight.round(2)} kg"
+else
+  puts 'Your weight is optimal'
+end
