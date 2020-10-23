5c247bd01d2136bdbd98d50aec78f3ea3feb3663
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-10-24 00:04:18 +0300

    area of triangle

---
+++ Lesson_1/triangle.rb
@@ -0,0 +1,9 @@
+print 'Enter base of triangle: '
+base = gets.chomp.to_i
+
+print 'Enter height of triangle: '
+height = gets.chomp.to_i
+
+area = base * height / 2.0
+
+puts "Area of triangle is #{area}"
