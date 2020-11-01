515f22db9507da25139c4db4050ade42c8418200
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-01 23:34:39 +0300

    add error next or previous

--- Lesson_3/train.rb
+++ Lesson_3/train.rb
@@ -16,14 +16,36 @@ class Train
   end
 
   def go_forvard
+    return if next_station.nil?
+
     @station.train_departure(self)
     @station = @route.stations[@index_station += 1]
     @station.train_arrival(self)
   end
 
   def go_back
+    return if previous_station.nil?
+
     @station.train_departure(self)
     @station = @route.stations[@index_station -= 1]
     @station.train_arrival(self)
+  end
+
+  private
+
+  def previous_station
+    if (@index_station - 1).negative?
+      puts 'Это первая станция в маршруте, предыдущей станции нет'
+    else
+      true
+    end
+  end
+
+  def next_station
+    if @route.stations.size - 1 < @index_station + 1
+      puts 'Это конечная станция в маршруте'
+    else
+      true
+    end
   end
 end
